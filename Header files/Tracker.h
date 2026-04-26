ab3ddcec438406ea9bc0c4072ae1594b9cc5a7d4
jahnaviigupta <jiyagupta0603@gmail.com>
2026-04-26 12:10:57 +0530

    Added Tracker class and addItem Function

---
+++ Header files/Tracker.h
@@ -0,0 +1,14 @@
+#ifndef TRACKER_H
+#define TRACKER_H
+#include "FoundItem.h"
+class Tracker{
+private:
+Item*items[100];
+int count;
+
+public: 
+Tracker();
+void addItem(Item* item);
+~Tracker();
+};
+#endif
