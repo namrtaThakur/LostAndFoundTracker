c4763e8a899506cef2cf5b28e020589d503641e9
namrtaThakur <namrtathakur488@gmail.com>
2026-05-01 21:20:21 +0530

    made compatible changes according to item header file

--- Source files/Item.cpp
+++ Source files/Item.cpp
@@ -1,7 +1,16 @@
 #include "Item.h"
+using namespace std;
 
 Item::Item(string n,string loc)
 {
     name= n;
     location= loc;
+}
+
+string Item::getName() const{
+    return name;
+}
+
+string Item::getLocation()const{
+    return location;
 }
