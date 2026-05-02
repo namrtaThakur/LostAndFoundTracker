024122eb492733d38665257ab1414026194490c3
jahnaviigupta <jiyagupta0603@gmail.com>
2026-05-02 22:39:18 +0530

    Implemented FoundItem.cpp

--- Source files/FoundItem.cpp
+++ Source files/FoundItem.cpp
@@ -1,13 +1,18 @@
 #include "FoundItem.h"
 using namespace std;
 
-FoundItem::FoundItem(string n, string loc, string date)
+FoundItem::FoundItem(string n, string loc, string date, string imgPath)
  :Item(n, loc),foundDate(date){}
 
 void FoundItem:: display() const{
  cout <<"Found Item:"<< name <<",Location:"<< location;
 if(!foundDate.empty())
  cout << ",Date:"<< foundDate;
+
+ if(!imagePath.empty())
+cout<<",Image:"<< imagePath;
+else
+cout<<",No image Provide";
 cout << endl;
 }
 string FoundItem::getFoundDate() const{
