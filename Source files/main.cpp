45d62ae84710b033216d9ce0d24cfd8c85a0d48e
jahnaviigupta <jiyagupta0603@gmail.com>
2026-05-02 22:52:33 +0530

    Changes made to integrate image in main

--- Source files/main.cpp
+++ Source files/main.cpp
@@ -1,75 +1,106 @@
-  #include <iostream>
-  #include"Tracker.h"
-  #include"LostItem.h"
-  #include"FoundItem.h"
+#include <iostream>
+#include "Tracker.h"
+#include "LostItem.h"
+#include "FoundItem.h"
 
-  using  namespace std;
+using namespace std;
 
-  int main()
-  { 
+int main()
+{
     Tracker tracker;
     int choice;
 
-    do{
-        cout <<\n--- Lost and Found Tracker ---\n";
-        cout <<1. Add lost item\n";
-        cout << 2. Add Found iten\n";
-        cout << 3. Display all Items\n";
-        cout << 4.Find Matches\n";
-        cout << "5.Search by name (Template)\n";
-        cout <<  "6. Exit\n";
-        cout << "Enter your Choice:";
+    do {
+        cout << "\n--- Lost and Found Tracker ---\n";
+        cout << "1. Add Lost Item\n";
+        cout << "2. Add Found Item\n";
+        cout << "3. Display All Items\n";
+        cout << "4. Find Matches\n";
+        cout << "5. Search by Name (Template)\n";
+        cout << "6. Exit\n";
+        cout << "Enter your choice: ";
         cin >> choice;
+        cin.ignore(); // IMPORTANT
 
-        if(choice ==1){
-            string name,location, date;
-            cout << "Enter item name:";
-            cin >> name;
-            cout << "Enter location:";
-            cin >> location;
-            cout << "Enter Lost date:";
-            cin >> date;"
+        if (choice == 1) {
+            string name, location, date, imagePath;
 
+            cout << "Enter item name: ";
+            getline(cin, name);
 
-            tracker.addItem(new LostItem(name, location, date));
-        } 
-        else if(choice == 2){
-            string name, location, date;
-            cout << "Enter item name:";
-            cin >> name;
-            cout << "Enter location:";
-            cin >> location;
-            cout << "Enter Found date:";
-            cin >> date;
+            cout << "Enter location: ";
+            getline(cin, location);
 
-            tracker.addItem(new FoundItem(name, location, date));
-    
+            cout << "Enter lost date: ";
+            getline(cin, date);
+
+            cout << "Enter image path (or press ENTER to skip): ";
+            getline(cin, imagePath);
+
+            tracker.addItem(new LostItem(name, location, date, imagePath));
+
+            // 🔥 Call matching immediately
+            LostItem temp(name, location, date, imagePath);
+            tracker.findMatches(temp);
         }
-        else if(choice == 3){
+
+        else if (choice == 2) {
+            string name, location, date, imagePath;
+
+            cout << "Enter item name: ";
+            getline(cin, name);
+
+            cout << "Enter location: ";
+            getline(cin, location);
+
+            cout << "Enter found date: ";
+            getline(cin, date);
+
+            cout << "Enter image path (or press ENTER to skip): ";
+            getline(cin, imagePath);
+
+            tracker.addItem(new FoundItem(name, location, date, imagePath));
+        }
+
+        else if (choice == 3) {
             tracker.displayAll();
+        }
 
-            
+        else if (choice == 4) {
+            string name, location, date;
+
+            cout << "Enter lost item details to search:\n";
+
+            cout << "Name: ";
+            getline(cin, name);
+
+            cout << "Location: ";
+            getline(cin, location);
+
+            cout << "Date: ";
+            getline(cin, date);
+
+            LostItem temp(name, location, date);
+            tracker.findMatches(temp);
         }
-        else if(choice == 4){
-            tracker.findMatches();
-        
-        }
-        else if(choice == 5){
+
+        else if (choice == 5) {
             string searchName;
-            cout << "Enter name to search:";
-            cin >> searchName;
-            tracker.searchByName(searchName);
-        
-        }
-        else if(choice == 6){
-            cout << "Invalid choice. Please try again.\n";
-            
-            
-        }
-        else if(choice ==7){
-            cout << "Exiting the program. Goodbye!\n";
+            cout << "Enter name to search: ";
+            getline(cin, searchName);
 
+            tracker.searchByName(searchName);
         }
-        return 0;
-        
-    } 
+
+        else if (choice == 6) {
+            cout << "Exiting...\n";
+        }
+
+        else {
+            cout << "Invalid choice. Please try again.\n";
+        }
+
+    } while (choice != 6);
+
+    return 0;
+}
