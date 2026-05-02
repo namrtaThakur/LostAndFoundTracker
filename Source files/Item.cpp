dbbd0254a0eab9e4a7d16c1e5a92defcdf944604
namrtaThakur <namrtathakur488@gmail.com>
2026-05-02 23:35:11 +0530

    image integration changes in item source file

--- Source files/Item.cpp
+++ Source files/Item.cpp
@@ -1,10 +1,11 @@
 #include "Item.h"
 using namespace std;
 
-Item::Item(string n,string loc)
+Item::Item(string n,string loc, string imgPath)
 {
     name= n;
     location= loc;
+    imagePath= imgPath;
 }
 
 string Item::getName() const{
@@ -13,4 +14,29 @@ string Item::getName() const{
 
 string Item::getLocation()const{
     return location;
+}
+
+void Item::setImagePath(const string& path)
+{imagePath = path;
+}
+
+string Item::getImagePath() const{
+    return imagePath;
+}
+
+bool Item::hasImage() const{
+    return !imagePath.empty();
+}
+
+void Item::openImage() const
+{
+    if (!imagePath.empty())
+{
+    string command = "start\"\"\"" + imagePath + "\"";
+    system(command.c_str());
+}
+else 
+{
+    cout<<"No image available. \n"
+}
 }
