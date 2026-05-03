090d0eb91a5158d29b0cb6078cf6c479c071562d
namrtaThakur <namrtathakur488@gmail.com>
2026-05-03 16:11:27 +0530

    changes made in tracker header file

--- Header files/Tracker.h
+++ Header files/Tracker.h
@@ -1,21 +1,23 @@
 #ifndef TRACKER_H
 #define TRACKER_H
 
-#include"item.h"
+#include "Item.h"
 #include "FoundItem.h"
+#include "LostItem.h"
 #include<string>
 class Tracker{
 private:
-Item*items[100];
+Item* items[100];
 int count;
 
 public: 
 Tracker();
 void addItem(Item* item);
 void displayAll() const;
-void findMatches() const;
+void findMatches(const LostItem& lost) const;
 template <typename T>
 void searchByName(T key)const;
+
 ~Tracker();
 };
 #endif
